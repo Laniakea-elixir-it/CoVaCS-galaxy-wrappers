61865c510cc345552a05388a7cac0fe329318d5a
pitta <pietro.mandreoli@gmail.com>
2018-10-03 16:29:53 +0200

    clinvar_update

--- annovar-wrapper/dwl_db_annovar_terminal.sh
+++ annovar-wrapper/dwl_db_annovar_terminal.sh
@@ -5,7 +5,7 @@ perl annotate_variation.pl  -downdb -buildver hg19 -webfrom annovar gnomad_genom
 perl annotate_variation.pl  -downdb -buildver hg19 -webfrom annovar exac03 humandb &&
 perl annotate_variation.pl  -downdb -buildver hg19 -webfrom annovar 1000g2015aug humandb &&
 perl annotate_variation.pl  -downdb -buildver hg19 -webfrom annovar avsnp150 humandb &&
-perl annotate_variation.pl  -downdb -buildver hg19 -webfrom annovar clinvar_20170905 humandb &&
+perl annotate_variation.pl  -downdb -buildver hg19 -webfrom annovar clinvar_20180603 humandb &&
 perl annotate_variation.pl  -downdb -buildver hg19 -webfrom annovar cosmic70 humandb &&
 perl annotate_variation.pl  -downdb -buildver hg19 -webfrom annovar dbnsfp33a humandb &&
 perl annotate_variation.pl  -downdb -buildver hg19 -webfrom annovar esp6500_all humandb &&
