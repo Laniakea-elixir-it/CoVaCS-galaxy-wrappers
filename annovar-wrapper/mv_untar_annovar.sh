7cf2a0d9de404b16a933eb6127e967923d7bd86b
pmandreoli <pietro.mandreoli@gmail.com>
2018-10-15 09:17:00 +0000

    generalization

--- annovar-wrapper/mv_untar_annovar.sh
+++ annovar-wrapper/mv_untar_annovar.sh
@@ -2,7 +2,7 @@
 #if the symbolic link is not present in the conda env of annovar the script search the tar.gz in the _conda prefix of the vm untar 
 #the archive and make a symbolic link of the directory in the conda env
 if [[ ! -d $CONDA_PREFIX/annovar ]] ; then
-	tar -zxvf $CONDA_PREFIX/../../annovar.latest.tar.gz -C $CONDA_PREFIX/../../ &&
+	tar -zxvf $CONDA_PREFIX/../../annovar.*.tar.gz -C $CONDA_PREFIX/../../ &&
 	echo start untar &&
 	ln -s $CONDA_PREFIX/../../annovar $CONDA_PREFIX &&
 	echo all done
