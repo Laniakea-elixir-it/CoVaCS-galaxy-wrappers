e5fca391a51c4bdd21574801a717f105135ae330
pmandreoli <pietro.mandreoli@gmail.com>
2018-10-15 09:26:28 +0000

    update

--- covacs_gatk/mv_untar_gatk.sh
+++ covacs_gatk/mv_untar_gatk.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #if the symbolic link is not present in the conda env of annovar the script search the tar.gz in the _conda prefix of the vm untar 
 #the archive and make a symbolic link of the directory in the conda env
-if [[ ! -d $CONDA_PREFIX/../../GenomeAnalysisTK.jar ]] ; then
-	tar -zxvf $CONDA_PREFIX/../../GenomeAnalysis*.tar.gz -C $CONDA_PREFIX/../../
+if [[ ! -d $CONDA_PREFIX/GenomeAnalysisTK.jar ]] ; then
+	tar -zxvf $CONDA_PREFIX/../../GenomeAnalysis*.tar.gz -C $CONDA_PREFIX/../../ &&
+	ln -s $CONDA_PREFIX/../../GenomeAnalysisTK.jar $CONDA_PREFIX
 	
 else
 	echo GATK was present
