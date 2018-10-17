dfa72fc363acb0b3f9268489d7e02326de4c874f
pmandreoli <pietro.mandreoli@gmail.com>
2018-10-17 11:40:01 +0000

    removed

--- covacs_gatk/mv_untar_gatk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#if the .jar file is not present in the conda_prefix the script search the tar.gz in the conda_prefix of the vm
-#and untar the archive
-if [[ ! -f $CONDA_PREFIX/../../GenomeAnalysisTK.jar ]] ; then
-	tar -zxvf $CONDA_PREFIX/../../GenomeAnalysis*.tar.gz -C $CONDA_PREFIX/../../ 
-	
-else
-	echo GATK is present
-fi
